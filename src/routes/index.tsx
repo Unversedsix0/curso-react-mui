7a60b44d758c871f53634afa4bfb33202fcdf1d8
Gabriel Pires <83356439+Unversedsix0@users.noreply.github.com>
2023-01-16 14:33:51 -0300

    feat: Adicionando tema na aplicação

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import Button from '@mui/material/Button'
 
 export const AppRoutes = () =>{
     return(
         <Routes>
-            <Route path='/pagina-inicial' element={<p>Pagina Inicial</p>}/>
+            <Route path='/pagina-inicial' element={<Button variant="contained" color="primary">TESTE</Button>}/>
 
             <Route path='*' element={<Navigate to={'/pagina-inicial'}/>}/>
         </Routes>
