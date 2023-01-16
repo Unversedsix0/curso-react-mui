d23640d11502b5f92779f8dc3c14a18f97d2e078
Gabriel Pires <83356439+Unversedsix0@users.noreply.github.com>
2023-01-16 14:18:58 -0300

    feat: criação do tema light

---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,24 @@
+import { createTheme} from '@mui/material';
+import {yellow,cyan} from '@mui/material/colors';
+
+export const LightTheme = createTheme({
+    pallete:{
+        primary:{
+            main: yellow[700],
+            dark: yellow[800],
+            light: yellow[500],
+            contrastText: '#ffffff'
+        },
+        secondary:{
+            main:cyan[500],
+            dark:cyan[400],
+            light: cyan[300],
+            contrastText: '#ffffff'
+        },
+        background:{
+            default: '#f7f6f3',
+            paper:'#ffffff' ,
+        },
+      
+    }
+});
