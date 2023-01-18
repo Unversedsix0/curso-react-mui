5fb4cb81554cc0ebf9b83410f286701811be9b9d
unversedsix0 <gaba.olipires@gmail.com>
2023-01-18 13:28:21 -0300

    feat: Adicionado botão "Pagina Inicial"

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Drawer, useTheme, Avatar, Divider } from '@mui/material'
+import { Drawer, useTheme, Avatar, Divider, List, ListItemButton, ListItemIcon, ListItemText,Icon } from '@mui/material'
 import { Box } from '@mui/material'
+import React from 'react'
 
 
 interface IMenuLateralProps {
@@ -21,20 +21,30 @@ export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
                 <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
 
                     <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
-                        <Avatar 
-                        sx={{
-                            height:theme.spacing(12),
-                            width:theme.spacing(12)
-                        
-                    
-                        }}
-                        src="http://disneyplusbrasil.com.br/wp-content/uploads/2022/12/Avatar-Loak.jpg" />
+                        <Avatar
+                            sx={{
+                                height: theme.spacing(12),
+                                width: theme.spacing(12)
+
+
+                            }}
+                            src="http://disneyplusbrasil.com.br/wp-content/uploads/2022/12/Avatar-Loak.jpg" />
                     </Box>
-                    <Divider/>
+                    <Divider />
                     <Box flex={1}>
+                        <List component="nav">
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Icon>
+                                        home
+                                    </Icon>
+                                </ListItemIcon>
+                                <ListItemText primary="Pagina Inicial" />
+                            </ListItemButton>
+                        </List>
 
                     </Box>
-                        
+
 
                 </Box>
 
