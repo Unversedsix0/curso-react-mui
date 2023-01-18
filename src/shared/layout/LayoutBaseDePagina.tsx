929f30f82b49dda3be93c8a6137930751a711a78
unversedsix0 <gaba.olipires@gmail.com>
2023-01-18 16:35:44 -0300

    feat: Melhorado titulo e botao para menu

--- src/shared/layout/LayoutBaseDePagina.tsx
+++ src/shared/layout/LayoutBaseDePagina.tsx
@@ -1,5 +1,8 @@
 import { Box } from '@mui/system'
 import React from 'react'
+import Typography from '@mui/material/Typography'
+import { useTheme, IconButton, Icon, useMediaQuery } from '@mui/material';
+import { useDrawerContext } from '../contexts';
 
 interface ILayoutBaseDePaginaProps {
     children: React.ReactNode
@@ -7,10 +10,23 @@ interface ILayoutBaseDePaginaProps {
 }
 
 export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = (props) => {
+    const theme= useTheme();
+    const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+    const {toggleDrawerOpen} = useDrawerContext();
+    
     return (
         <Box height="100%" display="flex" flexDirection="column" gap={1}>
-            <Box>
+            <Box padding={1} height={theme.spacing(12)} gap={1} display="flex" alignItems="center">
+               {smDown && (<IconButton onClick={toggleDrawerOpen}>
+                  <Icon>
+                    menu
+                  </Icon>
+                </IconButton>)}
+
+                <Typography variant='h5'>
                 {props.titulo}
+                </Typography>
+                
             </Box>
             <Box>
                 Barra de Ferramentas
