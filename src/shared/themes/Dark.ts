edb46aff1694472d731dfe371fc744b9ec2699da
Unversedsix0 <gaba.olipires@gmail.com>
2023-01-18 19:19:21 -0300

    Corrigido cor do texto

--- src/shared/themes/Dark.ts
+++ src/shared/themes/Dark.ts
@@ -1,24 +1,29 @@
-import { createTheme} from '@mui/material';
-import {yellow,cyan} from '@mui/material/colors';
+import { createTheme } from "@mui/material";
+import { yellow, cyan } from "@mui/material/colors";
 
 export const DarkTheme = createTheme({
-    palette:{
-        primary:{
-            main: yellow[700],
-            dark: yellow[800],
-            light: yellow[500],
-            contrastText: '#ffffff'
-        },
-        secondary:{
-            main:cyan[500],
-            dark:cyan[400],
-            light: cyan[300],
-            contrastText: '#ffffff'
-        },
-        background:{
-            default: '#202124',
-            paper:'#303134' ,
-        },
-      
+  palette: {
+    mode: "dark",
+    primary: {
+      main: yellow[700],
+      dark: yellow[800],
+      light: yellow[500],
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      main: cyan[500],
+      dark: cyan[400],
+      light: cyan[300],
+      contrastText: "#ffffff",
+    },
+    background: {
+      default: "#202124",
+      paper: "#303134",
+    },
+  },
+  typography: {
+    allVariants:{
+        color:'white'
     }
+  },
 });
